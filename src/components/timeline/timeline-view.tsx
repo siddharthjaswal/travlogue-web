8d1144a1c277ac51a28073c8834b9042a3317c16
Siddharth Jaswal <tosid23@gmail.com>
2026-02-16 18:17:53 +0000

    ui: add popup margins

--- src/components/timeline/timeline-view.tsx
+++ src/components/timeline/timeline-view.tsx
@@ -181,7 +181,7 @@ export function TimelineView({ tripId }: TimelineViewProps) {
                 </div>
 
                 <Dialog open={mapOpen} onOpenChange={setMapOpen}>
-                    <DialogContent className="!w-screen !h-screen !max-w-none !p-0 !rounded-3xl overflow-hidden flex flex-col">
+                    <DialogContent className="!w-[calc(100vw-48px)] !h-[calc(100vh-48px)] !max-w-none !p-0 !rounded-3xl overflow-hidden flex flex-col">
                         <div className="relative w-full h-full">
                             <div className="absolute inset-0">
                                 {trip && <TripMap trip={trip} height={0} className="h-full w-full" />}
