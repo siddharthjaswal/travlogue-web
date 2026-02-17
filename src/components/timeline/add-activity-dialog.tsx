1fe2c786bc3911e2648bdc1fea9eecc322b1d42e
Siddharth Jaswal <tosid23@gmail.com>
2026-02-17 07:25:00 +0000

    ui: move resolve button inline

--- src/components/timeline/add-activity-dialog.tsx
+++ src/components/timeline/add-activity-dialog.tsx
@@ -701,7 +701,7 @@ export function AddActivityDialog({
                                             control={form.control}
                                             name="endLocation"
                                             render={({ field }) => (
-                                                <FormItem>
+                                                <FormItem className="relative">
                                                     <FormLabel>End location</FormLabel>
                                                     <FormControl>
                                                         <Input
@@ -722,28 +722,27 @@ export function AddActivityDialog({
                                                             }}
                                                         />
                                                     </FormControl>
+                                                    {(form.watch('startLocation') && form.watch('endLocation')) && (
+                                                        <Button
+                                                            type="button"
+                                                            variant="outline"
+                                                            size="sm"
+                                                            className="absolute right-1 top-1/2 -translate-y-1/2 h-8"
+                                                            onClick={async () => {
+                                                                const start = form.getValues('startLocation');
+                                                                const endLoc = form.getValues('endLocation');
+                                                                if (start) await resolveLocationInput(start, setStartCoords);
+                                                                if (endLoc) await resolveLocationInput(endLoc, setEndCoords);
+                                                            }}
+                                                        >
+                                                            Resolve
+                                                        </Button>
+                                                    )}
                                                     <FormMessage />
                                                 </FormItem>
                                             )}
                                         />
                                         </div>
-                                        {(form.watch('startLocation') && form.watch('endLocation')) && (
-                                            <div className="flex justify-end">
-                                                <Button
-                                                    type="button"
-                                                    variant="outline"
-                                                    size="sm"
-                                                    onClick={async () => {
-                                                        const start = form.getValues('startLocation');
-                                                        const endLoc = form.getValues('endLocation');
-                                                        if (start) await resolveLocationInput(start, setStartCoords);
-                                                        if (endLoc) await resolveLocationInput(endLoc, setEndCoords);
-                                                    }}
-                                                >
-                                                    Resolve
-                                                </Button>
-                                            </div>
-                                        )}
                                     </>
                                 ) : (
                                     <FormField
